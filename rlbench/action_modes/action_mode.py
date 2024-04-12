fix typos and catch jacobian error

rlbench/action_modes/action_mode.py
@@ -2,7 +2,7 @@ from abc import abstractmethod
 
 import numpy as np
 
-from rlbench.action_modes.arm_action_modes import ArmActionMode
+from rlbench.action_modes.arm_action_modes import ArmActionMode, EndEffectorPoseViaPlanning
 from rlbench.action_modes.gripper_action_modes import GripperActionMode
 from rlbench.backend.scene import Scene
 
@@ -32,7 +32,10 @@ class MoveArmThenGripper(ActionMode):
         arm_action = np.array(action[:arm_act_size])
         ee_action = np.array(action[arm_act_size:arm_act_size+1])
         ignore_collisions = bool(action[arm_act_size+1:arm_act_size+2])
-        self.arm_action_mode.action(scene, arm_action, ignore_collisions)
+        if isinstance(self.arm_action_mode, EndEffectorPoseViaPlanning):
+            self.arm_action_mode.action(scene, arm_action, ignore_collisions)
+        else:
+            self.arm_action_mode.action(scene, arm_action)
         self.gripper_action_mode.action(scene, ee_action)
 
     def action_shape(self, scene: Scene):

rlbench/tasks/move_hanger.py
@@ -22,7 +22,7 @@ class MoveHanger(Task):
     def init_episode(self, index: int) -> List[str]:
         self.hanger.set_position(
             [self.initx + np.random.uniform(-0.1, 0.3), self.inity, self.initz])
-        return ['move hanger onto the other rack'
+        return ['move hanger onto the other rack',
                 'move the hanger from one rack to the other',
                 'put the hanger on the other rack',
                 'pick up the hanger and place it on the other rack']